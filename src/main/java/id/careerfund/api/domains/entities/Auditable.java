fbb4cc86ce3a3a93610b0f6c3bf89ddb8f7a1d35
mfahmialkautsar <mfahmiak@gmail.com>
2021-11-02 07:23:10 +0700

    Add entity audit

---
+++ src/main/java/id/careerfund/api/domains/entities/Auditable.java
@@ -0,0 +1,36 @@
+package id.careerfund.api.domains.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class Auditable<U> {
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private U createdBy;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private U updatedBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
