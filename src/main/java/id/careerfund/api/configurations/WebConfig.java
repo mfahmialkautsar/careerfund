34e76060d068cc51230cb1853b24356eed10ae3e
mfahmialkautsar <mfahmiak@gmail.com>
2021-11-11 22:01:53 +0700

    Reenable cors

---
+++ src/main/java/id/careerfund/api/configurations/WebConfig.java
@@ -0,0 +1,14 @@
+package id.careerfund.api.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
