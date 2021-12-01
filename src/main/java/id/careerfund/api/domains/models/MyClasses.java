63e7478afebf5fd2db7284808cf8cbf4c80695ad
mfahmialkautsar <mfahmiak@gmail.com>
2021-12-01 21:06:47 +0700

    Add my classes dummy API

---
+++ src/main/java/id/careerfund/api/domains/models/MyClasses.java
@@ -0,0 +1,12 @@
+package id.careerfund.api.domains.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class MyClasses {
+    private List<MyClass> data;
+}
