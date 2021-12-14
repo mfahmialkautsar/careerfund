90302124bbd61a1f76c193c26f2818b2f9fe2f7b
mfahmialkautsar <mfahmiak@gmail.com>
2021-12-14 19:00:35 +0700

    Update Loan API response from Page to List

--- src/main/java/id/careerfund/api/controllers/LoanController.java
+++ src/main/java/id/careerfund/api/controllers/LoanController.java
@@ -5,13 +5,13 @@ import id.careerfund.api.domains.entities.Loan;
 import id.careerfund.api.domains.models.ApiResponse;
 import id.careerfund.api.services.LoanService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,17 +20,17 @@ public class LoanController extends HandlerController {
 
     @Secured({ERole.Constants.LENDER})
     @GetMapping("/lender/loans")
-    public ResponseEntity<ApiResponse<Page<Loan>>> getLenderMyLoans(
+    public ResponseEntity<ApiResponse<List<Loan>>> getLenderMyLoans(
             Principal principal
     ) {
-        return ResponseEntity.ok(ApiResponse.<Page<Loan>>builder().data(loanService.getLoans(null, null)).build());
+        return ResponseEntity.ok(ApiResponse.<List<Loan>>builder().data(loanService.getLoans(null, null).getContent()).build());
     }
 
     @Secured({ERole.Constants.LENDER})
     @GetMapping("/lender/my/loans")
-    public ResponseEntity<ApiResponse<Page<Loan>>> getLenderLoans(
+    public ResponseEntity<ApiResponse<List<Loan>>> getLenderLoans(
             Principal principal
     ) {
-        return ResponseEntity.ok(ApiResponse.<Page<Loan>>builder().data(loanService.getMyLoans(principal, null, null)).build());
+        return ResponseEntity.ok(ApiResponse.<List<Loan>>builder().data(loanService.getMyLoans(principal, null, null).getContent()).build());
     }
 }
