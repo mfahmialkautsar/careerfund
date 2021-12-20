6eedeacfb1f2cbe78b55e26d59b3981fc85b1a33
mfahmialkautsar <mfahmiak@gmail.com>
2021-12-20 16:47:10 +0700

    JWT error handling

--- src/main/java/id/careerfund/api/utils/filters/JwtFilter.java
+++ src/main/java/id/careerfund/api/utils/filters/JwtFilter.java
@@ -3,9 +3,13 @@ package id.careerfund.api.utils.filters;
 import id.careerfund.api.domains.models.JwtConfig;
 import id.careerfund.api.services.JwtServiceImpl;
 import id.careerfund.api.services.UserServiceImpl;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -31,27 +35,52 @@ public class JwtFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        String token = null;
-        String email = null;
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+        try {
+            String token = null;
+            String email = null;
 
-        String authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader != null && authorizationHeader.startsWith(jwtConfig.getTokenPrefix())) {
-            token = authorizationHeader.substring(7);
-            email = jwtServiceImpl.extractUsername(token);
-        }
+            String authorizationHeader = request.getHeader("Authorization");
+            if (authorizationHeader != null && authorizationHeader.startsWith(jwtConfig.getTokenPrefix())) {
+                token = authorizationHeader.substring(7);
+                email = jwtServiceImpl.extractUsername(token);
+            }
 
-        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = userService.loadUserByUsername(email);
-            if (jwtServiceImpl.validateToken(token, userDetails)) {
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
-                usernamePasswordAuthenticationToken
-                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userService.loadUserByUsername(email);
+                if (jwtServiceImpl.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken
+                            .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
+            filterChain.doFilter(request, response);
+        } catch (MalformedJwtException | SignatureException e) {
+            response.setContentType("application/json");
+            try {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token is not as expected");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            }
+        } catch (InvalidBearerTokenException e) {
+            response.setContentType("application/json");
+            try {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token has expired");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setContentType("application/json");
+            try {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "There is something wrong with your JWT");
+            } catch (IOException ex) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             }
         }
-        filterChain.doFilter(request, response);
     }
 }
